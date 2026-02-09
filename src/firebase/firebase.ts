211da0c3b0a08532085d492327d8a3671fa66719
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2026-02-09 21:48:03 +0530

    configured firebase google authentication

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,20 @@
+// src/firebase.ts  (or firebase/config.ts)
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHARa7hf6trrMmDk5fT6Nc9m9jo8hkCVc",
+  authDomain: "authenticate-28507.firebaseapp.com",
+  projectId: "authenticate-28507",
+  storageBucket: "authenticate-28507.firebasestorage.app",
+  messagingSenderId: "447939440659",
+  appId: "1:447939440659:web:27fb78b808c42dfae68fe6",
+  measurementId: "G-NP91K3F74M"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
